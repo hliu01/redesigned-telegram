f083282b920c436c58cdd5e9300ab947138e8b19
Henry Liu <hliu01@stuy.edu>
2020-04-30 17:56:42 -0400

    try

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
+@app.route("/ok")
+def cool():
+    return render_template("ok.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
