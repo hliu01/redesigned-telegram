4c0199322063fdabce2341d42c05b25fc3e2af4d
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2020-05-03 13:56:44 -0400

    added documentation to graph.js

--- static/js/graph.js
+++ static/js/graph.js
@@ -6,6 +6,13 @@ class Graph {
 		this.margins = margins;
 	};
 
+	/**
+	 * @param {Object[]} data - dataset, where each row is given by a prototype `{parameter: value ...}`
+	 * @param {Object} params - prototype describing the variables to graph
+	 * @param {string} params.x
+	 * @param {string} params.y
+	 * @returns {{x: Number[], y: Number[]}} Domain of x and y
+	 */
 	static domain(data, params) {
 		return {
 			x: d3.extent(data, (datum) => {return datum[params.x]}),
@@ -13,17 +20,26 @@ class Graph {
 		};
 	};
 
+	/**
+	 * @param {Number[]} domain - array of the form `[min, max]`, which does not neccessarily need to be a `Number[]`, can be a `Date`.\
+	 * Accepts `[min, max]` to return a function which scales an input to fit within `[min, max]`.
+	 */
 	scale_horizontal(domain) {
 		let span = this.width - (this.margins.left + this.margins.right);
+		/** @param {Number} value */
 		let scaler = function(value) {
 			let ratio = (value - domain[0]) / (domain[1] - domain[0]);
 			return span * ratio;
 		};
 		return scaler;
 	};
-
+	/** Accepts `[min, max]` to return a function which scales an input to fit within `[max, min]`.\
+	 * Flipped to fit the inversion of vertical coordiantes in HTML.
+	 * @param {Number[]} domain - array of the form `[min, max]`, which does not neccessarily need to be a `Number[]`, can be a `Date`.\
+	 */
 	scale_vertical(domain) {
 		let span = this.height - (this.margins.top + this.margins.bottom);
+		/** @param {Number} value */
 		let scaler = function(value) {
 			let ratio = (value - domain[0]) / (domain[1] - domain[0]);
 			return span * (1 - ratio);
@@ -33,10 +49,19 @@ class Graph {
 };
 
 class LineGraph extends Graph {
+
+	/** Uses dataset to generate points as zero-length lines
+	 * @param {Object[]} data - dataset, where each row is given by a prototype `{parameter: value ...}`
+	 * @param {string} name - name of line, for internal use (conventionally, the name of the dataset)
+	 * @param {Object} params - prototype describing the variables to graph
+	 * @param {string} params.x
+	 * @param {string} params.y
+	 */
 	generate_points(data, name, params) {
 		let domain = Graph.domain(data, params);
 		let scale_x = (datum) => {return this.scale_horizontal(domain.x)(datum[params.x]);};
 		let scale_y = (datum) => {return this.scale_vertical(domain.y)(datum[params.y]);};
+
 		this.display.append("g").attr("class", `lines ${name}`)
 			.attr("transform", `translate(${GRAPH.margins.left},${GRAPH.margins.bottom})`)
 			.selectAll("dot").data(data).enter().append("line")
@@ -45,6 +70,10 @@ class LineGraph extends Graph {
 			.style("stroke", "black").style("opacity", 1);
 	};
 
+	/** Animates drawing of line graph
+	 * @param {string} name - Name of line group (set by `generate_points`) to animate
+	 * @param {Number} duration - Total duration of graphing animation in miliseconds
+	 */
 	animate_lines(name, duration) {
 		let lines = this.display.node()
 			.querySelectorAll(`.${name}`)[0]
@@ -59,6 +88,14 @@ class LineGraph extends Graph {
 		}
 	};
 
+	/** Graphs a dataset. Wrapper for `generate_points` and `animate_lines`.
+	 * @param {Object[]} data - dataset, where each row is given by a prototype `{parameter: value ...}`
+	 * @param {string} name - name of line, for internal use (conventionally, the name of the dataset)
+	 * @param {Object} params - prototype describing the variables to graph
+	 * @param {string} params.x
+	 * @param {string} params.y
+	 * @param {Number} duration - Total duration of graphing animation in miliseconds
+	 */
 	graph(data, name, params, duration) {
 		this.generate_points(data, name, params);
 		this.animate_lines(name, duration);
