aadb2b90a3a40ee8c57261b0027aa5c9b50dc440
clement6chan <cchan00@stuy.edu>
2020-05-10 17:11:17 -0400

    move dataMaker to utl

---
+++ app/utl/dataMaker.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+def makeData(dataType, country, startDate, endDate, cell):
+    if startDate < "2020-01-22" and endDate > "2020-04-25":
+        makeData(dataType, country, "2020-01-22", "2020-04-25", cell)
+    elif startDate < "2020-01-22":
+        makeData(dataType, country, "2020-01-22", endDate, cell)
+    elif endDate > "2020-04-25":
+        makeData(dataType, country, startDate, "2020-04-25", cell)
+    else:
+        if dataType == '1':
+            currency(country, startDate, endDate, cell)
+        else:
+            covid(country, startDate, endDate, cell)
+
+
+def covid(country, startDate, endDate, cell):
+    db = sqlite3.connect("data/boop.db")
+    c = db.cursor()
+    print('covid ' + startDate + ":" + endDate)
+    idx = c.execute('SELECT id FROM countries WHERE name="{}";'.format(country)).fetchall()[0][0]
+    dataset = open('data/dataset{}.csv'.format(cell), 'w')
+    dataset.write('country,date,confirmed,deaths,recovered\n')
+    arr = c.execute('SELECT date, confirmed, deaths, recovered FROM data WHERE countryId="{}";'.format(idx)).fetchall()
+    for entry in arr:
+        if startDate <= entry[0] <= endDate:
+            line = country
+            for item in entry:
+                line += "," + str(item)
+            dataset.write(line + "\n")
+    dataset.close()
+
+
+def currency(country, startDate, endDate, cell):
+    db = sqlite3.connect("data/boop.db")
+    c = db.cursor()
+    print('currency ' + startDate + ":" + endDate)
+    dataset = open('data/dataset{}.csv'.format(cell), 'w')
+    dataset.write('base,date,USD,EUR,GBP,JPY,CNY\n')
+    arr = c.execute("SELECT * FROM currency WHERE base='{}';".format(country)).fetchall()
+    arr.sort()
+    for entry in arr:
+        if startDate <= entry[1] <= endDate:
+            line = ""
+            for item in entry:
+                line += "," + str(item)
+            dataset.write(line[1:] + "\n")
+    dataset.close()
